56daf462689265c896a0dd2d33e26a7bee94dc2e
Alexander Hsiao <hsiao.alexander@gmail.com>
2018-06-13 09:53:06 +0200

    Install pods: SwiftyJSON, Chameleon Framework, Foursquare, ProgressHUD

---
+++ Immedia Places/Controller/MapController.swift
@@ -0,0 +1,19 @@
+//
+//  MapController.swift
+//  Immedia Places
+//
+//  Created by Alexander on 2018/06/13.
+//  Copyright © 2018 Alexander Hsiao. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
